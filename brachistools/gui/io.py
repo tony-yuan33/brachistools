eb661f41855dd3937769446bc7dbaf95ba8ab2a0
Ruihong Yuan <ruihong.19@intl.zju.edu.cn>
2023-12-12 00:08:04 +0800

    Add load_folder

--- brachistools/gui/io.py
+++ brachistools/gui/io.py
@@ -2,3 +2,10 @@
 import os, gc
 from natsort import natsorted
 import numpy as np
+
+
+def load_folder(path, file_ext = "") -> 'list[str]':
+    """Get all file names with specified extension under a folder"""
+    files = filter(lambda fn: fn.endswith(file_ext), os.listdir(path))
+    return natsorted(files)
+
