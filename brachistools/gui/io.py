f66d901a84a31bdcf1f6ec03179ac35c6ab61ac9
Ruihong Yuan <ruihong.19@intl.zju.edu.cn>
2023-12-15 21:50:24 +0800

    Support reading multiple file extensions

--- brachistools/gui/io.py
+++ brachistools/gui/io.py
@@ -1,11 +1,25 @@
 
 import os, gc
+from typing import Iterable
+
 from natsort import natsorted
 import numpy as np
 
+from ..io import imread, imsave
 
-def load_folder(path, file_ext = "") -> 'list[str]':
-    """Get all file names with specified extension under a folder"""
-    files = filter(lambda fn: fn.endswith(file_ext), os.listdir(path))
-    return natsorted(files)
+def load_folder(path, file_ext) -> 'list[str]':
+    """Get all file names with specified extension(s) under a folder"""
+    if isinstance(file_ext, Iterable):
+        file_ext = list(ext.lower() for ext in file_ext)
+    else:
+        file_ext = [file_ext.lower()]
+
+    files = os.listdir(path)
+    def get_results():
+        for file in files:
+            for ext in file_ext:
+                if file.lower().endswith(ext):
+                    yield file
+
+    return natsorted(get_results())
 
